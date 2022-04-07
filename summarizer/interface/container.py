d135ea219cd3feb22e575489bccf05723a007396
minsu <minsu2530@u.sogang.ac.kr>
2022-04-08 00:58:58 +0900

    redis -> event

--- summarizer/interface/container.py
+++ summarizer/interface/container.py
@@ -10,7 +10,7 @@ class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
     redis = providers.Singleton(Redis, host=config.redis_host, port=config.redis_port)
 
-    redis_listener = providers.Factory(RedisListener, redis=redis, key=config.redis_key)
+    event_listener = providers.Factory(RedisListener, redis=redis, key=config.redis_key)
     
     dynamodb = boto3.resource('dynamodb', endpoint_url=config.dynamodb_url)
     
